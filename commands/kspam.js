da078db7110de6756ec857e4b24aff18af92aa42
HB <62563173+HBPlayz@users.noreply.github.com>
2021-02-19 17:40:22 +0000

    i am femboy

--- commands/kspam.js
+++ commands/kspam.js
@@ -4,13 +4,13 @@ module.exports = {
     guildOnly: true,
     execute(message, args){
         if (!args.length) {
-        return message.channel.send(enter a game pin dummy);
+        return message.channel.send('enter a game pin dummy');
     } else {
 
-    var varPin = '${args}'
-            const KahootSpam = require('kahoot-spam')
-            let api = KahootSpam
-            api.spam(varPin, "amogusdrip", 156)
-}
+    		var varPin = ${args}
+        	const KahootSpam = require('kahoot-spam')
+        	let api = KahootSpam
+        	api.spam(varPin, "amogusdrip", 156)
+	}
     }
 }
