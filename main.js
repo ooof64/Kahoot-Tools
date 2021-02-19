c1829d258a915baae29e9af4338e9a968df39aae
ooof64 <71406172+ooof64@users.noreply.github.com>
2021-02-19 17:00:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -35,4 +35,4 @@ client.on('message', message => {
 });
 
 //Add to server link: https://tinyurl.com/y4ah2h6o
-client.login('TOKEN');
+client.login('ODEyMzY3NDcxMTU0OTU0MzIx.YC_uKg.RP2SmYfQtZFwNBrTp3GUh64_cQc');
