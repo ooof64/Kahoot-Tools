077435a4e48493e406ae10c214750da4932760f9
ooof64 <71406172+ooof64@users.noreply.github.com>
2021-02-19 17:26:16 +0000

    Update kspam.js

--- commands/kspam.js
+++ commands/kspam.js
@@ -3,9 +3,14 @@ module.exports = {
     description: "kspam!",
     guildOnly: true,
     execute(message, args){
-        var varPin = 
-        const KahootSpam = require('kahoot-spam')
-        let api = KahootSpam
-        api.spam(varPin, "amogusdrip", 156)
+        if (!args.length) {
+        return message.channel.send(enter a game pin dummy);
+    } else {
+
+    var varPin = '${args}'
+            const KahootSpam = require('kahoot-spam')
+            let api = KahootSpam
+            api.spam(varPin, "amogusdrip", 156)
+}
     }
 }
