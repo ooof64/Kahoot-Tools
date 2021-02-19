a9cad436fb3759918f157da0aa123270484385aa
HB <62563173+HBPlayz@users.noreply.github.com>
2021-02-19 17:51:13 +0000

    Update kspam.js

--- commands/kspam.js
+++ commands/kspam.js
@@ -4,13 +4,12 @@ module.exports = {
     guildOnly: true,
     execute(message, args){
         if (!args.length) {
-        return message.channel.send('enter a game pin dummy');
-    } else {
-
-    		var varPin = ${args}
-        	const KahootSpam = require('kahoot-spam')
-        	let api = KahootSpam
-        	api.spam(varPin, "amogusdrip", 156)
+          return message.channel.send('enter a game pin dummy');
+    	} else {
+    	  var varPin = ${args}
+          const KahootSpam = require('kahoot-spam')
+          let api = KahootSpam
+          api.spam(varPin, "amogusdrip", 256)
 	}
     }
 }
