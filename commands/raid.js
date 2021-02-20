add7b73102947545a5c60304f9851f3b393747ac
HB <62563173+HBPlayz@users.noreply.github.com>
2021-02-20 13:52:02 +0000

    fixed console spam and logged spamming target

--- commands/raid.js
+++ commands/raid.js
@@ -7,11 +7,11 @@ module.exports = {
           return message.channel.send('you need to enter the game pin e.g. k!raid 381928');
       	} else {
     	    var varPin = `${args}`
-          console.log('Raid started by user ' + message.member.id)
-	        message.channel.send('Sending raid bots to ' + `${args}`)
+          console.log('Raid started by user ' + message.member.id  + ' \(targeting game: ' + `${args}` + '\)')
+	      message.channel.send('Sending raid bots to ' + `${args}`)
           const KahootSpam = require('kahoot-spam')
           let api = KahootSpam
-          api.spam(varPin, "RAIDED", 256)
+          api.spam(varPin, "RAIDED", 256).catch(() => {})
         }
     }
 }
