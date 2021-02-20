a257604a52f1f9c5368af9055b0f3c053646821b
ooof64 <71406172+ooof64@users.noreply.github.com>
2021-02-20 08:36:38 +0000

    Update kspam.js

--- commands/kspam.js
+++ commands/kspam.js
@@ -1,13 +1,13 @@
 module.exports = {
-    name: 'kspam',
-    description: "kspam!",
+    name: 'raid',
+    description: "raid!",
     guildOnly: true,
     execute(message, args){
         if (!args.length) {
-          return message.channel.send('enter a game pin dummy');
+          return message.channel.send('you need to enter the game pin e.g. k!raid 381928');
     	} else {
     	  var varPin = `${args}`
-	  message.channel.send(ok im gonna spam kahoot `${args}`)
+	  message.channel.send(Sending raid bots to `${args}`)
           const KahootSpam = require('kahoot-spam')
           let api = KahootSpam
           api.spam(varPin, "amogusdrip", 256)
