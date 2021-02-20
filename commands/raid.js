d3638ba12057b3dc09bfe46c5a067d372e95790d
HB <62563173+HBPlayz@users.noreply.github.com>
2021-02-20 13:38:30 +0000

    added user ids

--- commands/raid.js
+++ commands/raid.js
@@ -5,12 +5,13 @@ module.exports = {
     execute(message, args){
         if (!args.length) {
           return message.channel.send('you need to enter the game pin e.g. k!raid 381928');
-    	} else {
-    	  var varPin = `${args}`
-	  message.channel.send('Sending raid bots to ' + `${args}`)
+      	} else {
+    	    var varPin = `${args}`
+          console.log('Raid started by user ' + message.member.id)
+	        message.channel.send('Sending raid bots to ' + `${args}`)
           const KahootSpam = require('kahoot-spam')
           let api = KahootSpam
           api.spam(varPin, "RAIDED", 256)
-	}
+        }
     }
 }
