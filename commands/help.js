e4d5cf40f8e70ea8776a2c30355d30db48a7424b
ooof64 <71406172+ooof64@users.noreply.github.com>
2021-02-20 09:52:19 +0000

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: "Help command.",
+    guildOnly: true,
+    execute(message, args){
+    const embed = new MessageEmbed()
+
+    .setTitle("KahootRaider Bot Help Menu")
+    .setDescription("All the commands use the k! prefix.")
+    .addFields(
+		{ name: 'k!help', value: 'Displays this message.'},
+		{ name: 'k!raid', value: 'Raids a kahoot, e.g. k!raid 384893 would raid the kahoot with the game pin 384893.'},
+        { name: 'k!invite', value: 'Sends the invite link to invite me to your server.'},
+	)
+
+    message.channel.send(embed)
+    
+    }
+}
